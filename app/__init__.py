0a6c57d2ba2acd099898c19a244854273287e802
Jack <stryker2k2@msn.com>
2025-10-30 20:47:15 +0000

    Dynamically list Proxmox Container & Dynamically create Proxmox Container & List Classes from JSON & Flask Flash

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, Blueprint, session
 from app.main import main
+from app.extensions import celery_init_app
 import logging, uuid, os
 from datetime import datetime
 
@@ -9,10 +10,13 @@ logger = logging.getLogger(__name__)
 def create_app():
     app = Flask(__name__)
     app.config.from_prefixed_env()
-    app.config['SESSION_COOKIE_SECURE'] = True
+    # app.config['SESSION_COOKIE_SECURE'] = True
     app.config["DEBUG"] = True
     app.config["START_TIME"] = datetime.now()
-    app.secret_key = "CaptureTheFlag"
+    app.config["CELERY"] = {"broker_url": "redis://localhost:6379", "result_backend": "redis://localhost:6379"}
+    app.config["SECRET_KEY"] = "CaptureTheFlag"
+
+    celery_init_app(app)
 
     app.register_blueprint(main)
 
--- app/main.py
+++ app/main.py
@@ -1,17 +1,31 @@
-from flask import Flask, render_template, Blueprint
-import logging
+from flask import Flask, render_template, Blueprint, redirect, url_for, request, flash
+import logging, json
+from pathlib import Path
+from .proxmox import getContainers, createTarget
 
 main = Blueprint("main", __name__)
-
 logger = logging.getLogger(__name__)
+
+classes_path = "app/data/classes.json"
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    with open(classes_path, 'r') as file:
+        data = json.load(file)
+    return render_template('index.html', data=data)
 
 @main.route('/machines')
 def machines():
-    return render_template('machines.html')
+    containers = getContainers()
+    return render_template('machines.html', containers=containers)
+
+@main.route("/spinup", methods=["POST", "GET"])
+def spinup():
+    if request.method == "POST":
+        msg = createTarget()
+        flash(msg)
+        return redirect(url_for('main.machines'))
+    return "<h1>ERROR: 404</h1>"
 
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=5000, debug=True)
